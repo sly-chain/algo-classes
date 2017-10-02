b598ac8b7ec2031fcf493708bda0a996866c59e1
slysylvia <sylviaxh@gmail.com>
2017-10-01 22:38:07 -0700

    homework 2 completed

--- homework2.py
+++ homework2.py
@@ -7,11 +7,18 @@ ith row of the file indicates the ith entry of an array.
 Because of the large size of this array, you should implement the fast 
 divide-and-conquer algorithm covered in the video lectures."""
 
+def create_integer_array(filename):
+    with open(filename) as f:
+        integer_array = list(map(int, f.read().splitlines()))
+
+    return integer_array
+
+
 def splitArray(integer_array):
     
-    mid = integer_array // 2
-    array_a = mergeSort(integer_array[:mid])
-    array_b = mergeSort(integer_array[mid:])   
+    mid = len(integer_array) // 2
+    array_a = integer_array[:mid]
+    array_b = integer_array[mid:] 
     
     return array_a, array_b
 
@@ -61,40 +68,60 @@ def mergeSort(sub_array):
 
 def counting_inversions(integer_array):
     
+    inv_list = []
+    
     array_a, array_b = splitArray(integer_array)
     
     sorted_a = mergeSort(array_a)
     sorted_b = mergeSort(array_b)
-    
-    sorted_list = []
     i = 0
     j = 0
+    k = 0
     inv = 0   
     
-    if not len(sorted_a):
-        sorted_list.append(sorted_b[j])
-        j += 1
-    
-    for i in len(range(sorted_a)):
-        if sorted_a[i] < sorted_b[j]:
-            sorted_list.append(sorted_a[i])
-            i += 1
-        else:
-            sorted_list.append(sorted_b[j])
+    for k in range(len(integer_array)):
+        if len(integer_array) == 1:
+            return 0
+        
+        while i < len(sorted_a) and j < len(sorted_b):
+            if sorted_a[i] < sorted_b[j]:
+                integer_array[k] = sorted_a[i]
+                i += 1
+            else: 
+                integer_array[k] = sorted_b[j]
+                j += 1
+                inv += len(sorted_a) - i
+                inv_list.append(len(sorted_a) - i)
+            k += 1
+            
+        while j < len(sorted_b):
+            integer_array[k] = sorted_b[j]
             j += 1
-            inv += 1
+            k += 1
+            
+        while i < len(sorted_a):
+            integer_array[k] = sorted_a[i]
+            i += 1
+            k += 1
+     
+    return integer_array, inv, inv_list, sorted_a, sorted_b
+        
 
-    return sorted_list, inv 
+    
+integer_array = create_integer_array('IntegerArray.txt')
+#integer_array = [1, 3, 5, 2, 4, 6]
+#integer_array = [8, 12, 3, 10, 15, 18]
+sorted_list, inv, inv_list, sorted_a, sorted_b = counting_inversions(integer_array)
+print('result', inv)
+    
+file = open('results.txt', 'w')
+file.write(str(inv_list))
+file.close()
 
+file = open('sub_array.txt', 'w')
+file.write(str(sorted_a))
+file.close()
 
-    
-#integer_array = 
-#counting_inversions(integer_array)
-    
-    
-    
-    
-    
-    
-    
+#4,999,950,000
+
     
