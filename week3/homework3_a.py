edad7161d239b33f12548b1c338a6515c52f98c4
slysylvia <sylviaxh@gmail.com>
2017-11-02 13:38:34 -0700

    still confused how to add up comparisons without it resetting when function gets to base case

--- week3/homework3_a.py
+++ week3/homework3_a.py
@@ -12,13 +12,13 @@ other m−1 elements in the subarray in this recursive call.)
 # first element of the array as the pivot element
 
 def partition(input_array, left_bound, right_bound):
-    if len(input_array) == 1:
+    if len(input_array) == 0 or len(input_array) == 1:
         return 0, input_array
     
     pivot = input_array[left_bound]
     i = left_bound + 1
 
-    for j in range(left_bound + 1, right_bound):
+    for j in range(i, right_bound):
         if input_array[j] < pivot:
             input_array[i], input_array[j] = input_array[j], input_array[i]
             i += 1
@@ -28,35 +28,33 @@ def partition(input_array, left_bound, right_bound):
 
 
 
-def sort(partitioned_array, sorted_array=[], comparisons=0):
+def sort(partitioned_array, sorted_array=[], comparisons = 0):
     len_array= len(partitioned_array)
     
     if len_array == 1:
+        print('1', comparisons)
         return sorted_array.append(partitioned_array[0])
     
     else:
-        
         k = 0
-        print('array length', len_array-1)
+        comparisons += len_array-1
         
         for j in range(len_array-1):
-            
-            
             if partitioned_array[k] > partitioned_array[j+1]:
                 partitioned_array[j+1], partitioned_array[k] = partitioned_array[k], partitioned_array[j+1]
                 k += 1
-             
-#            print(j, comparisons)
+            
         sorted_array.append(partitioned_array[0])
         sort(partitioned_array[1:])
-    comparisons += (len_array-1)   
+        
+    print('2', comparisons)
     return comparisons, sorted_array
 
 
 
 def quick_sort(input_array):
     
-    if len(input_array) == 0:
+    if len(input_array) == 0 or len(input_array) == 1:
         return 0, input_array
     
     pivot, partitioned_array = partition(input_array, left_bound, right_bound)
@@ -71,7 +69,7 @@ def quick_sort(input_array):
 
             
         
-
+comparisons = 0
 input_array = [3, 8, 2, 5, 1, 4, 7, 6]
 left_bound = 0 
 right_bound = len(input_array) - 1
