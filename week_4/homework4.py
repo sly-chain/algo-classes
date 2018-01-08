5dd9f00f6d4a9a27f602ea2572f0566e563aa9c5
sly-chain <sylviaxh@gmail.com>
2018-01-07 21:23:40 -0800

    i think the karger algo is working

--- week_4/homework4.py
+++ week_4/homework4.py
@@ -20,59 +20,60 @@ provided.
 
 import random
 
-class Graph(object):
-    def __init__(self, file):
-        self._file = file
 
+# create represenation of graph as dictionary
+def create_graph(file):
+    graph = {}
+    
+    with open(file) as adjacency_list:
+        for line in adjacency_list:
+            single_line = [int(s) for s in line.split()]
+            graph[single_line[0]] = single_line[1:]
+            
+    return graph
 
-    def _create_graph(self):
-        graph = {}
-        
-        with open(self._file) as adjacency_list:
-            for line in adjacency_list:
-                single_line = [int(s) for s in line.split()]
-                graph[single_line[0]] = single_line[1:]
-                
-        return graph
-
-graph = Graph._create_graph('week_4/kargerMinCut.txt')
 
 # choose random edge 
-def get_random_edge():
+def get_random_edge(current_graph):
     """
     pick random key
     then pick random value from that key
     """
-    v1 = random.choice(list(graph))
-    v2 = random.choice(v1)
+    v1 = random.choice(list(current_graph))
+    v2 = random.choice(current_graph[v1])
     
     return v1, v2
-        
-# contract chosen edge
-def contract():
-    v1, v2 = get_random_edge()
-
+  
+      
+# contract chosen random edge
+def contract(current_graph):
+    v1, v2 = get_random_edge(current_graph)
+    
   # add vertices from graph[v2] to graph[v1]
   # delete graph[v2]
-    graph[v1].extend[graph(v2)]
-    del graph[v2]
+    updated_graph = current_graph.copy()
+    updated_graph[v1].extend(updated_graph[v2])
+    del updated_graph[v2]
     
   # delete any references to v2  
-    for k, v in graph.iteritems():
-        graph[k].remove(v2)
-  
+    for k, v in updated_graph.items():
+        updated_graph[k] = [v1 if x == v2 else x for x in updated_graph[k]]
+    
   # remove self loops
-    for v in graph[v1]:
-        if v == v1:
-            graph[v1].remove(v1)
-    
+    updated_graph[v1] = [x for x in updated_graph[v1] if x!= v1]
+    print(v1, v2, updated_graph[v1], '\n')
 
-def min_cut():
-    while len(graph) <2:
-        contract()
-    # count the edges
-    
+    return updated_graph
 
-# repeat algorithm N times
+
+def min_cut(current_graph):
+    
+    while len(current_graph) > 2:
+        current_graph = contract(current_graph)
         
+    return len(current_graph)
+    
+
+graph = create_graph('kargerMinCut.txt')
+print(min_cut(graph))   
 
