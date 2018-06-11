35a8477598c6d551ce03b8558a570a7c7a80a7e2
sly-chain <sylviaxh@gmail.com>
2018-06-11 01:01:58 -0700

    still working on kruskal and parent/rank denotion

--- 3-greedy_algorithms_minimum_spanning_trees_and_dynamic_programming/week_1/homework1d.py
+++ 3-greedy_algorithms_minimum_spanning_trees_and_dynamic_programming/week_1/homework1d.py
@@ -29,6 +29,7 @@ def create_graph(file):
     with open(file) as adjacency_list:
         first_line = adjacency_list.readline()
         next(adjacency_list)
+        
         for line in adjacency_list:
             single_line = [int(s) for s in line.split()]
             vertex = single_line[0]
@@ -42,34 +43,62 @@ def create_graph(file):
 
 
 
-def parent_update(parent_map, vertex):
-    if parent_map[vertex] == vertex:
-        return vertex
-    return parent_update(parent_map, parent_map[vertex])
+
+def find_parent(v):
+    if parent[v] != v:
+        find_parent(parent[v])
+    return parent[v]
+
+
+def update_parent(v, n):
+    v_root = find_parent(v)
+    n_root = find_parent(n)
+    
+    if rank[n_root] < rank[v_root]:
+        parent[n_root] = v_root
+        
+    else:
+        parent[v_root] = n_root
+        if rank[v_root] == rank[n_root]:
+            rank[v_root] += 1
+
 
 
 def kruskal():
-    parent_map = {elem:elem for elem in range(1, (graph_details[0] + 1))}
+    iteration = 0
+    i = 0
     result = []
     
-    for subset in graph:
-#        print(subset)
+#    while iteration < graph_details[0]-1:
+#        print('iteration', iteration)
+    while iteration < graph_details[0]-1:
+        print(iteration)
+        subset = graph[i]
+        i += 1
+        root_1 = find_parent(subset[1])
+        root_2 = find_parent(subset[2])
+#        print('roots', root_1, root_2)
         
-        if parent_map[subset[1]] != parent_map[subset[2]]:
-            result.append(subset)
-            parent_map[subset[2]] = parent_update(parent_map, subset[1])
-#            parent_map[subset[1]] = parent_update(parent_map, subset[1])
-#        print(parent_map[subset[1]], parent_map[subset[2]])
-    
-    print(parent_map)
-    
-    return len(result)
+        if root_1 != root_2:
+            result.append(subset[0])
+            iteration += 1
+            update_parent(subset[1], subset[2])
+#            print('parent', parent, '\n', 'rank', rank)
+            
+    return sum(result)
     
 
 graph_details, graph = create_graph('edges.txt')    
+parent = {elem:elem for elem in range(1, graph_details[0] + 1)}
+rank = {elem:0 for elem in range(1, graph_details[0] + 1)}
 print(kruskal())
-    
-    
+
+
+#graph_details = [5, 10]
+#graph = [[4, 2, 3], [5, 0, 3], [6, 0, 2], [10, 0, 1], [15, 1, 3]]
+#parent = [elem for elem in range (0,4)]
+#rank = [0 for elem in range (0,4)]
+#print(kruskal())
     
     
     
