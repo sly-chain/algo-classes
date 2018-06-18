f374875a40d7d57c5ce619bbb71051ea2c61d39f
sly-chain <sylviaxh@gmail.com>
2018-06-17 22:56:54 -0700

    homework 2 using kruskalesque approach.

--- 3-greedy_algorithms_minimum_spanning_trees_and_dynamic_programming/week_2/homework2.py
+++ 3-greedy_algorithms_minimum_spanning_trees_and_dynamic_programming/week_2/homework2.py
@@ -16,7 +16,7 @@ edge costs). It has the following format:
 
 ...
 
-There is one edge (i,j) for each choice of 1 ≤ i < j ≤ n, where nnn is the 
+There is one edge (i,j) for each choice of 1 ≤ i < j ≤ n, where n is the 
 number of nodes.
 
 For example, the third line of the file is "1 3 5250", indicating that the 
@@ -29,4 +29,85 @@ this data set, where the target number k of clusters is set to 4. What is the
 maximum spacing of a 4-clustering?
 
 """
+
+
+import numpy as np
+import itertools
+import time
+
+class Graph:
+
+    def __init__(self, file, k):
+        self.file = file
+        self.k = k
+        self.clusters = {}
+        self.rank = {}
+        self.graph_details, self.graph = self.create_graph()
+
+
+    def create_graph(self):
+        edges_graph = []
+        
+        with open(self.file) as adjacency_list:
+            first_line = adjacency_list.readline()
+            graph_details = int(first_line)
+    
+            for line in adjacency_list:
+                single_line = [int(s) for s in line.split()]
+                vertex = single_line[0]
+                node = single_line[1]
+                cost = single_line[2]
+                
+                edges_graph.append([(vertex, node), cost])
+        
+        return graph_details, sorted(edges_graph, key=lambda x: x[1])
+    
+    
+    def find_centroid(self, n):
+        for centroid, node in self.clusters.items():   
+            if n in node:
+                return centroid
+    
+    
+    def find_distance(self, e):
+        for subset in self.graph:
+            if (e[0], e[1]) in subset:
+                return subset[1]
+    
+    
+    def distance_matrix(self):
+        distances = []
+        matrix = np.array(list(itertools.permutations(self.clusters.keys(), 2)))
+        
+        for i in matrix:
+            distances.append(self.find_distance(i))
+        
+        return max(x for x in distances if x is not None)
+    
+
+    def k_means(self):
+        self.clusters = {elem:[elem] for elem in range(1, self.graph_details+1)}
+        i=0
+        
+        for subset in self.graph:
+            
+            centroid_1 = self.find_centroid(subset[0][0])
+            centroid_2 = self.find_centroid(subset[0][1])
+            
+            if len(self.clusters) == self.k:
+                break
+            
+            if centroid_1 != centroid_2:
+                self.clusters[centroid_1].extend(self.clusters[centroid_2])
+                del self.clusters[centroid_2]
+                i += 1
+                
+        return self.distance_matrix()
+            
+        
+g = Graph('clustering1.txt', 4) 
+start_time = time.time()
+print(g.k_means())
+print("--- %s seconds ---" % (time.time() - start_time))
+# 7503
 
