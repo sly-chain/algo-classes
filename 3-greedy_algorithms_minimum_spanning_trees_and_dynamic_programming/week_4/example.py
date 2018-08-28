bea5dc7bc7bc903cda6fe1fa0e328f010848fcbf
sly-chain <sylviaxh@gmail.com>
2018-08-27 20:47:01 -0700

    still trying to figure out memoization

--- 3-greedy_algorithms_minimum_spanning_trees_and_dynamic_programming/week_4/example.py
+++ 3-greedy_algorithms_minimum_spanning_trees_and_dynamic_programming/week_4/example.py
@@ -1,21 +1,49 @@
+import functools
+
+# =============================================================================
+# def create_graph(file):
+#     with open(file) as f:
+#         first_line = f.readline()
+#         graph_details = [int(s) for s in first_line.split()]
+# #        graph = [map(int, line.split()) for line in f]
+#         graph = list(map(int, line.split())) for line in f
+#     print(graph)
+#     return graph_details, graph
+# =============================================================================
+
 def create_graph(file):
-    with open(file) as f:
-        first_line = f.readline()
+    graph = []
+    
+    with open(file) as adjacency_list:
+        first_line = adjacency_list.readline()
         graph_details = [int(s) for s in first_line.split()]
-        graph = [map(int, line.split()) for line in f]
             
+        for line in adjacency_list:
+            single_line = [int(s) for s in line.split()]
+            graph.append(single_line)
+    
     return graph_details, graph
 
 
-def memoized(f):
-    memo_dict = {}
+# =============================================================================
+# def memoized(f):
+#     memo_dict = {}
+# 
+#     def helper(x):
+#         if x not in memo_dict:            
+#             memo_dict[x] = f(x)
+#         return memo_dict[x]
+#     return helper
+# =============================================================================
 
-    def helper(x):
-        if x not in memo_dict:            
-            memo_dict[x] = f(x)
-        return memo_dict[x]
-    return helper
-
+def memoized(obj):
+    cache = obj.cache = {}
+    @functools.wraps(obj)
+    def memoizer(*args, **kwargs):
+        if args not in cache:
+            cache[args] = obj(*args, **kwargs)
+        return cache[args]
+    return memoizer
 
 
 def knapsack():
@@ -41,8 +69,12 @@ def knapsack():
     # elements in items whose weights sum to no more than j.
     @memoized
     def bestvalue(i, j):
-        if i == 0: return 0
+        if i == 0: 
+            return 0
+        
         value, weight = graph[i - 1]
+#        value = graph[i-1][0]
+#        weight = graph[i-1][1]
         if weight > j:
             return bestvalue(i - 1, j)
         else:
@@ -50,13 +82,13 @@ def knapsack():
                        bestvalue(i - 1, j - weight) + value)
 
     j = graph_details[0]
-    result = []
+#    result = []
     for i in range(graph_details[1], 0, -1):
         if bestvalue(i, j) != bestvalue(i - 1, j):
-            result.append(graph[i - 1])
+#            result.append(graph[i - 1])
             j -= graph[i - 1][1]
-    result.reverse()
-    return bestvalue(graph_details[1], graph_details[0]), result
+#    result.reverse()
+    return bestvalue(graph_details[1], graph_details[0])
 
 
 import time
@@ -68,11 +100,11 @@ start_time = time.time()
 #2493893
 #--- 1.253927230834961 seconds ---
 
-#graph_details, graph = create_graph('knapsack_big.txt') 
+graph_details, graph = create_graph('knapsack_big.txt') 
 #4243395
 #--- 5076.164584875107 seconds ---
 
-graph_details, graph = create_graph('test_cases/test1.txt') 
+#graph_details, graph = create_graph('test_cases/test1.txt') 
 #147
 #--- 0.002542734146118164 seconds ---
 
