d5a2337e1a154a57d556a95f5bb7c11b132e8178
sly-chain <sylviaxh@gmail.com>
2018-12-15 21:35:55 -0800

    still working on the scc method

--- 4-shortest_paths_revisited_NP/week_4/homework4.py
+++ 4-shortest_paths_revisited_NP/week_4/homework4.py
@@ -1,5 +1,5 @@
 '''
-The file format is as follows. In each instance, the number of variables and the number of clauses is the same, and this number is specified on the first line of the file. Each subsequent line specifies a clause via its two literals, with a number denoting the variable and a "-" sign denoting logical "not". For example, the second line of the first data file is "-16808 75250", which indicates the clause x{16808} V x{75250}.
+The file format is as follows. In each instance, the number of variables and the number of clauses is the same, and this number is specified on the first line of the file. Each subsequent line specifies a clause via its two literals, with a number denoting the variable and a "-" sign denoting logical "not". For example, the second line of the first data file is "-16808 75250", which indicates the clause -x{16808} V x{75250}.
 
 Your task is to determine which of the 6 instances are satisfiable, and which are unsatisfiable. In the box below, enter a 6-bit string, where the ith bit should be 1 if the ith instance is satisfiable, and 0 otherwise. For example, if you think that the first 3 instances are satisfiable and the last 3 are not, then you should enter the string 111000 in the box below.
 
@@ -13,25 +13,27 @@ class Graph():
     
     def __init__(self, file):
         self.file = file
-        self.graph = defaultdict(list)
-        self.reverse_graph = defaultdict(list)
+        self.graph, self.reverse_graph = self.create_graph()
         self.total_count = 0
         
-    def add_clause(self, x, y, total_count):
-        if x < 0:
-            x += total_count 
-        if y < 0:
-            y ++ total_count
-        self.graph[x].append(y)
-        
-    def add_inverse(self, x, y, total_count):
+    def add_clause(self, x, y, total_count, graph):
         if x < 0:
             x += total_count 
         if y < 0:
             y += total_count
-        self.reverse_graph[y].append(x)
+        graph[x].append(y)
+        
+    def add_inverse(self, x, y, total_count, reverse_graph):
+        if x < 0:
+            x += total_count 
+        if y < 0:
+            y += total_count
+        reverse_graph[y].append(x)
 
     def create_graph(self):
+        graph = defaultdict(list)
+        reverse_graph = defaultdict(list)
+        
         with open(self.file) as adjacency_list:
             first_line = adjacency_list.readline()
             self.total_count = int(first_line)
@@ -41,72 +43,69 @@ class Graph():
                 x = single_line[0]
                 y = single_line[1]
                 
-                self.add_clause(x, y, self.total_count)
-                self.add_inverse(x, y, self.total_count)
+                self.add_clause(x, y, self.total_count, graph)
+                self.add_inverse(x, y, self.total_count, reverse_graph)
                 
+        return graph, reverse_graph
+            
     
-def dfs(total_count):
+def dfs():
     visited = []
     stack = []
     
-    for node in range(total_count):
+    for node in g.graph:
         if node in visited:
             continue
         dfs_helper(node, visited, stack)
-    print(stack)
+
     return stack
     
 def dfs_helper(node, visited, stack):
     visited.append(node)
     
-    for v in g.graph[node]:
-        if v in visited:
-            continue
-        dfs_helper(v, visited, stack)
+    if node in g.graph:
+    
+        for v in g.graph[node]:
+            if v in visited:
+                continue
+            dfs_helper(v, visited, stack)
+            
     stack.append(node)
-    print('here')
-
-
+    
+    
 def reverse_dfs(stack):
     visited = []
+    scc = {}
     counter = 0
-    scc = []
     
     while stack:
         node = stack.pop(0)
-    
-        if node in visited:
-            continue    
-        reverse_dfs_helper(node, visited, counter)
+        reverse_dfs_helper(node, visited, scc, counter)
         counter += 1
-
-    return scc
     
-def reverse_dfs_helper(node, visited, counter):
+#    print(scc)
+    return scc
+
+def reverse_dfs_helper(node, visited, scc, counter):
     visited.append(node)
 
-    for v in g.reverse_graph[node]:
-        if v in visited:
-            continue
-        reverse_dfs_helper(v, visited)
-    
+    if node in g.reverse_graph:
+        for v in g.reverse_graph[node]:
+            if v in visited:
+                continue
+            reverse_dfs_helper(v, visited, scc, counter)
+        
     scc[node] = counter
-    
 
-def scc(g):
-    stack = dfs(g.total_count)
+
+def sat():
+    stack = dfs()
     scc = reverse_dfs(stack)
     
     for i in range(g.total_count):
         if scc[i] == scc[i + g.total_count]:
             return 0
     return 1
-        
-
-
-
-
-
 
 
 
@@ -122,6 +121,8 @@ start_time = time.time()
 
 
 #g = Graph('2sat1.txt')
+#--- 420.0536332130432 seconds ---
+
 #g = Graph('2sat2.txt')
 #g = Graph('2sat3.txt')
 #g = Graph('2sat4.txt')
@@ -129,7 +130,7 @@ start_time = time.time()
 #g = Graph('2sat6.txt')
 
 
-g = Graph('test_cases/test1.txt')
+#g = Graph('test_cases/test1.txt')
 # =============================================================================
 #0
 
@@ -137,9 +138,9 @@ g = Graph('test_cases/test1.txt')
 # =============================================================================
 #1
 
-#g = Graph('test_cases/test3.txt')
+g = Graph('test_cases/test3.txt')
 # =============================================================================
-#1
+#0
 
-print(scc(g))
+print(sat())
 print("--- %s seconds ---" % (time.time() - start_time)) 
